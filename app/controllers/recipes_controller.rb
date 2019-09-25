c4b2ecdeebcbfa380a656b084a32978852b0cfba
Ahmed Abououf <aabuauf@yahoo.com>
2019-09-25 22:39:06 +0000

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,11 @@
+require 'pry'
+
 class RecipesController < ApplicationController
   def show
+
     @recipe = Recipe.find(params[:id])
+        
+
   end
 
   def index
@@ -9,8 +14,30 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build()
+    @recipe.ingredients.build()
+    # @person.addresses.build(address_type: 'work')
+    # @person.addresses.build(address_type: 'home')
   end
 
   def create
+      recipe = Recipe.create(recipe_params)
+       
+    redirect_to recipe
   end
+ 
+ private
+def recipe_params
+  params.require(:recipe).permit(
+    :title,
+    ingredients_attributes: [
+      :id,
+      :name,
+      :quantity
+    ]
+  )
 end
+
+end
+
+
